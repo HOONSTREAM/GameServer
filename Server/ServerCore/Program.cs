1c0e601173feea546fbf377dc568e65463b0e1fb
SEUNG_HOON_LEE <123620703+HOONSTREAM@users.noreply.github.com>
2024-02-11 18:29:01 +0900

    24.02.11 UPDATED

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,38 +1,39 @@
 ﻿namespace ServerCore
 {
+
+    //강의의핵심내용 : 디버그모드에서 코드를 설계 후 버그가 일어나지 않지만, 릴리즈모드로 가면서 컴파일러 최적화 때문에 버그가 발생하는 경우가 있다 .
+
     class Program
     {
-        static void MainThread(object state)
-        {
-            for(int i = 0; i<3;i++)
-            Console.WriteLine("Hello Thread");
-        }
-        static void Main(string[] args)
-        {
 
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(5, 5);
+        volatile static bool _stop = false; // 전역일때는 스레드가 공유하여 동시접근이 가능함.
+        //volatile ; 코드상에서는 최적화 하지 말아주세요 키워드 , 최신값을 가져와라의 의미 
 
-            for(int i = 0; i<5; i++)
+        static void ThreadMain()
+        {
+            Console.WriteLine("쓰레드 시작!");
+
+            while(_stop == false)
             {
-                Task t = new Task(() => { while(true); }, TaskCreationOptions.LongRunning);
-                t.Start();
+
             }
 
-            //for(int i = 0; i<5 ; i++)
-            //{
-            //    ThreadPool.QueueUserWorkItem((obj) => { while (true); });
-            //}
+            Console.WriteLine("쓰레드 종료!");
+        }
+       
+        static void Main(string[] args)
+        {
+            Task t = new Task(ThreadMain);
+            t.Start();
 
-            ThreadPool.QueueUserWorkItem(MainThread);
+            Thread.Sleep(1000); // 밀리세컨드 단위
 
+            _stop = true;
 
-            //Thread t = new Thread(MainThread);
-            //t.Name = "Test Thread";
-            //t.Start();
-            //t.Join();
-
-            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("stop 호출");
+            Console.WriteLine("종료 대기중");
+            t.Wait(); // 스레드는 Join 
+            Console.WriteLine("종료 성공");
         }
     }
 }
